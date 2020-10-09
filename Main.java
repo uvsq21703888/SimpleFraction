2ad9209d69597dabc67cfdb9df9ff47151bc291c
LIM Jeff <jeff.lim@ens.uvsq.fr>
2020-10-09 17:17:26 +0200

    Add a constructor in Fraction class and assert in Main

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
+import Fraction.java;
 
 public class Main {
 
   public static void main(String[] args) {
     System.out.println("Hello World !");
+
+    Fraction test = new Fraction (1, 3);
+     assertEquals(test.toString(), "1/3");
+
+  }
 }
